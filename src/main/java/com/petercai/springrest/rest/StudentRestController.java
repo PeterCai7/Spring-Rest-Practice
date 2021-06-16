37e4b7159440c23ff9ad4379a55aaff582538e08
PeterCai7 <caiju33@sina.com>
2021-06-16 12:41:15 -0400

    Path variables

--- src/main/java/com/petercai/springrest/rest/StudentRestController.java
+++ src/main/java/com/petercai/springrest/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.petercai.springrest.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +16,29 @@ import com.petercai.springrest.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	// define endpoint for "/students" 
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		List<Student> theStudents = new ArrayList<>();
+	private List<Student> theStudents;
+	
+	//define @PostConstruct to load data
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<>();
 		theStudents.add(new Student("User", "One"));
 		theStudents.add(new Student("User", "Two"));
 		theStudents.add(new Student("User", "Three"));
+	}
+	
+	// define endpoint for "/students" 
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 		return theStudents;
 	}
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		//keep it simple for now
+		
+		return theStudents.get(studentId);
+	}
+	
 }
